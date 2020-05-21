009db5f1129c3ad37fca0c2acbad9da04aac2ef6
fuhuan1991 <huanfu2019@gmail.com>
2020-05-21 09:49:57 -0400

    new portfolio

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import 'antd/dist/antd.css';
+import Main from './main/Main';
+
+ReactDOM.render(<Main />, document.getElementById('root'));
+
+document.title = 'My Website';
