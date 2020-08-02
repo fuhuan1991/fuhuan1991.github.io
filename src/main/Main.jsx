37ab0e69468e36c1f810e2d2c239276ca891389c
fuhuan1991 <huanfu2019@gmail.com>
2020-08-02 19:01:57 -0400

    added a new project

--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -16,6 +16,11 @@ import g3 from './img/g3.png';
 import e1 from './img/e1.png';
 import e2 from './img/e2.png';
 import e3 from './img/e3.png';
+import m1 from './img/m1.png';
+import m2 from './img/m2.png';
+import m3 from './img/m3.png';
+import m4 from './img/m4.png';
+import m5 from './img/m5.png';
 
 
 class Main extends Component {
@@ -44,27 +49,68 @@ class Main extends Component {
       <div className='main'>
         <h1 className='white_font' 
           style={{textAlign: 'center', marginTop: '40px'}}>
-            Hello, my name is Huan.
+            Hello, I'm Huan Fu
         </h1>
         <h1 className='white_font' 
           style={{textAlign: 'center'}}>
-            I am a programmer.
+            I am a programmer
         </h1>
         <h1 className='white_font' style={{marginBottom: '0px'}}>ABOUT ME</h1>
         <hr className='line'/>
         <p className='white_font'>
-          I am a master’s student in computer science at Tufts and am very passionate in applying
-          programing skills to create better experience for users. I have 2-year full time 
-          working experience as a front-end engineer before joining Tufts last year. I have tackled 
+          Enthusiastic Software Engineer with 2 years of developing experience of data platform and E-commerce web applications, 
+          specialized in Front-end Development, API design, Microservice and Database.
+          Currently, I'm a master’s student in computer science at Tufts and am very passionate in applying
+          programing skills to create better experience for users. I have tackled 
           complex technical problems and demonstrated strong programming skills at Lenovo and GrowingIO.
-          Last semester, I built a mobile attendance system for faculties based on android system.
-          Now, I am working on several different projects. 
+          Last Summer, I worked as a software engineer intern at MilliporeSigma.&nbsp;&nbsp;
+          <a className='info_link' href='https://github.com/fuhuan1991'>My GitHub</a>
         </p>
 
         <h1 className='white_font' style={{marginBottom: '0px'}}>PROJECTS</h1>
         <hr className='line'/>
 
-        <h1 className='white_font sub_title'>1. GrowingIO.com</h1>
+        <h1 className='white_font sub_title'>1. BrightLab Customer Management Plan <a className='info_link' href='https://www.brightlab.com/'>About BrightLab</a><br/> </h1>
+        <div className='slider'>
+          <Carousel ref={node => (this.carousel = node)} autoplay>
+            <img src={m1} alt='cannot be displayed due to network reasons'></img>
+            <img src={m2} alt='cannot be displayed due to network reasons'></img>
+            <img src={m3} alt='cannot be displayed due to network reasons'></img>
+            <img src={m4} alt='cannot be displayed due to network reasons'></img>
+            <img src={m5} alt='cannot be displayed due to network reasons'></img>
+          </Carousel>
+        </div>
+        <h2 className='white_font'>About this project</h2>
+        <hr className='line'/>
+        <p className='white_font'>
+          BrightLab, a cloud-based research platform that brings people, projects and consumables together to optimize workflow and accelerate discovery. 
+          My task is to enhance the BrightLab Admin, a platform to manage all the customer information on BrightLab. 
+          I added a new lab dashboard module to BrightLab Admin which allows us to view and controll lab specific data like inventory, personnel, equipment and activities.
+          This greatly reduced the manual effort from back-end team. The new module also contains several data visualization features. 
+          They provide insights, makes it possible to track how users are using their labs and also enable business leaders to make more informed decisions.
+        </p>
+        <h2 className='white_font'>My role</h2>
+        <ul className='list'>
+          <li>Built administration platform to manage customer lab information, minimizing manual effort</li>
+          <li>Designed a data dashboard with data visualization features to facilitate more informed business decisions</li>
+          <li>Developed GraphQL service to provide easy-to-access data for front-end</li>
+        </ul>
+        <h2 className='white_font' style={{marginBottom: '0px'}}>Tech stack</h2>
+        <p className='white_font'>Code technologies involved while developing this project.</p>
+        <hr className='line'/>
+        <ul className='list'>
+          <li>React</li>
+          <li>GraphQL</li>
+          <li>Node.js</li>
+          <li>MongoDB</li>
+          <li>Docker</li>
+          <li>Kubernetes</li>
+        </ul>
+
+
+
+
+        <h1 className='white_font sub_title'>2. GrowingIO.com <a className='info_link' href='https://www.growingio.com/'>Visit this site</a><br/> </h1>
         <div className='slider'>
           <Carousel ref={node => (this.carousel = node)} autoplay>
             <img src={p0} alt='cannot be displayed due to network reasons'></img>
@@ -77,17 +123,17 @@ class Main extends Component {
         <h2 className='white_font'>About this project</h2>
         <hr className='line'/>
         <p className='white_font'>
-          GrowingIO is a leading analytic platform provider in China. They help customer track data, building 
+          GrowingIO is a leading analytic platform provider in China. They help customer tracking data, building 
           dashboard and drive quick insights. I was part of the platform team and our goal is to ensure that 
-          users (enterprise user) can manage their data easily.&nbsp;&nbsp;
-          <a className='info_link' href='https://www.growingio.com/'>Visit this site</a><br/>
-          My role in the team:<br/>
+          users (enterprise user) can customize all sorts of analytic plans easily. This enables them to track how 
+          the customers are using their websites.&nbsp;&nbsp;
         </p>
-          <ul className='list'>
-            <li>Designed a new data management module.</li>
-            <li>Refactored old react components to improve cohesion</li>
-            <li>Developed a react component library for company’s common components.</li>
-          </ul>
+        <h2 className='white_font'>My role</h2>
+        <ul className='list'>
+          <li>Developed new features for online data analytic platform that caters to over 7,000 corporate users</li>
+          <li>Designed and developed a data management module which enables customization of user behavior analytic plan</li>
+          <li>Developed a front-end component library for developing team</li>
+        </ul>
         <h2 className='white_font' style={{marginBottom: '0px'}}>Tech stack</h2>
         <p className='white_font'>Code technologies involved while developing this project.</p>
         <hr className='line'/>
@@ -98,13 +144,14 @@ class Main extends Component {
           <li>Node.js</li>
           <li>Javascript ES6 & Typescript</li>
           <li>CSS3 & less</li>
-          <li>Webpack</li>
+          <li>Webpack & Rollup</li>
+          <li>Docker</li>
         </ul>
 
 
 
         <h1 className='white_font sub_title'>
-          2. Chalkboard Note &nbsp;
+          3. Chalkboard Note &nbsp;
         </h1>
         <div className='slider'>
           <Carousel ref={node => (this.carousel = node)} autoplay>
@@ -117,7 +164,7 @@ class Main extends Component {
         <h2 className='white_font'>About this project</h2>
         <hr className='line'/>
         <p className='white_font'>
-          This is a online note management app, you can create, store and modify your notes.<br />
+          This is one of my personal projects. It's an online note management application, you can create, store and modify your notes.<br />
           There is a default account with some existing notes, name: guest, password: 123.<br />
         </p>
         <h2 className='white_font' style={{marginBottom: '0px'}}>Tech stack</h2>
@@ -138,7 +185,7 @@ class Main extends Component {
 
 
         <h1 className='white_font sub_title'>
-          3. Reversi Game &nbsp;
+          4. Reversi Game &nbsp;
           <a className='info_link' href='https://fuhuan1991.github.io/Reversi-Game/index.html'>Try it!</a>
         </h1>
         {/* <img src={gameImg} alt='cannot be displayed due to network reasons'></img> */}
@@ -153,7 +200,7 @@ class Main extends Component {
         <hr className='line'/>
         <ul className='list'>
           <li>
-          This is a little game based on Reversi.
+          This is one of my personal projects, a little fun game based on Reversi.
           <a className='info_link' href='https://en.wikipedia.org/wiki/Reversi'>(What is Reversi?)</a>
           </li>
           <li>Players take turns placing pieces on the board. After a move, any pieces of the opponent's color 
@@ -180,7 +227,7 @@ class Main extends Component {
 
 
         <h1 className='white_font sub_title'>
-          4. EasyPoll &nbsp;
+          5. EasyPoll &nbsp;
           <a className='info_link' href='http://easypoll.us-east-2.elasticbeanstalk.com'>Try it!</a>
         </h1>
         {/* <img src={gameImg} alt='cannot be displayed due to network reasons'></img> */}
@@ -198,6 +245,7 @@ class Main extends Component {
           <li>No need to create an account.</li>
           <li>All you need to do is: Setup questions(single choice or multiple choice), share links with your friends and 
             watch the result.</li>
+          <li>Available for both PC and mobile</li>
         </ul>
         <h2 className='white_font' style={{marginBottom: '0px'}}>Tech stack</h2>
         <p className='white_font'>Code technologies involved while developing this project.</p>
